d24f236f906ad59f1a74c0db132a28e4fd1a5cc6
Jørgen Verhaeren <65420814+jjaverhaeren@users.noreply.github.com>
2020-09-19 23:06:34 +0200

    add Opdracht10 2.0
    Worked out Guess the Game with working attempt count.

--- Opdracht10/script2.js
+++ Opdracht10/script2.js
@@ -1,28 +1,62 @@
 let name = prompt(`Hallo! Welkom bij  WHAT's THE MAGIC NUMBER! Hoe heet je?`);
 let min = prompt(
-  `Geef een min en een max waartussen we het Magic Number gaan raden. Voer eerst het kleinste getal in en druk op OK om verder te gaan.`
+  `Hallo ${name}. Geef een min en een max waartussen we het Magic Number gaan raden. Voer eerst het kleinste getal in en druk op OK om verder te gaan.`
 );
 let max = prompt(`Voer het grootste getal in.`);
+Math.floor(Math.random() * (max - min + 1)) + min;
+let maxAttempts = 5;
+let attempt = 1;
 
-let num = prompt(
-  `Wat is volgens jou het Magic Number? Voer een heel getal in.`
-);
+const guess = function () {
+  if (maxAttempts - attempt === -1) {
+    endGame();
+  } else {
+    num = prompt(
+      `Wat is volgens jou het Magic Number? Voer een heel getal in.`
+    );
+    if (num === magicNum) {
+      win();
+    } else {
+      lose();
+    }
+  }
+};
 
-// let roundedNum = Math.round(num);
-
-let magicNum = Math.floor(Math.random() * (max - min + 1) + min);
-alert(`${magicNum} was het te raden Magic Number...`);
-
-if (num !== magicNum) {
-  alert(
-    `Sorry ${name}. ${roundedNum} was dus niet het Magic Number. Je mag nog een keer proberen.`
-  );
-} else {
-  win();
-}
+const lose = function () {
+  if (maxAttempts - attempt === 0) {
+    alert(`Sorry ${name}. ${num} was ook niet het Magic Number.`);
+    endGame();
+  } else {
+    alert(
+      `Sorry ${name}. ${num} was niet het Magic Number. Je mag nog ${
+        maxAttempts - attempt
+      } keer raden.`
+    );
+    attempt++;
+    guess();
+  }
+};
 
 const win = () => {
   alert(
     `WOW ${name}! Je hebt het Magic Number geraden! Je bent hier echt goed in! Druk op OK en daarna cmd-R om nog eens te spelen.`
   );
 };
+
+const game = function () {
+  if (attempt < maxAttempts) {
+    guess();
+  } else {
+    alert(
+      `Sorry ${name}. Je mag niet meer raden. Volgende keer beter! Druk OK en vervolgens cmd-R om nog eens te spelen.`
+    );
+  }
+};
+
+const endGame = function () {
+  alert(
+    `Het Magic Number was ${magicNum}! Dankjewel voor het spelen van Guess the Magic Number. druk op OK en vervolgens cmd-R om nog een keer te spelen. `
+  );
+};
+
+game();
